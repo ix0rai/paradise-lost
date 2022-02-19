5b6e71465692d1aef6857d68cb2d06a3941c2029
24Chrome <m.batt1@outlook.com>
2022-02-19 17:40:31 -0600

    Wisteria Tree refactoring
    ONLY SMALL WISTERIAS AT THE MOMENT

--- src/main/java/net/id/aether/world/feature/tree/placers/WisteriaTrunkPlacer.java
+++ src/main/java/net/id/aether/world/feature/tree/placers/WisteriaTrunkPlacer.java
@@ -2,6 +2,7 @@ package net.id.aether.world.feature.tree.placers;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import it.unimi.dsi.fastutil.ints.IntSets;
 import net.id.aether.util.AStarManager;
 import net.id.aether.world.feature.tree.AetherTreeHell;
 import net.minecraft.block.BlockState;
@@ -16,6 +17,7 @@ import net.minecraft.world.gen.foliage.FoliagePlacer;
 import net.minecraft.world.gen.trunk.TrunkPlacer;
 import net.minecraft.world.gen.trunk.TrunkPlacerType;
 
+import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -23,28 +25,29 @@ import java.util.function.BiConsumer;
 
 public class WisteriaTrunkPlacer extends TrunkPlacer {
 
+    /*
+     * https://www.desmos.com/calculator/n4q9yugst4
+     */
+
     public static final Codec<WisteriaTrunkPlacer> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            IntProvider.VALUE_CODEC.fieldOf("tries").forGetter(placer -> placer.tries),
-            IntProvider.VALUE_CODEC.fieldOf("xOffset").forGetter(placer -> placer.xOffset),
-            IntProvider.VALUE_CODEC.fieldOf("yOffset").forGetter(placer -> placer.yOffset),
-            IntProvider.VALUE_CODEC.fieldOf("zOffset").forGetter(placer -> placer.zOffset),
-            FloatProvider.VALUE_CODEC.fieldOf("secondaryGrowthChance").forGetter(placer -> placer.secondaryGrowthChance),
+            IntProvider.VALUE_CODEC.fieldOf("maxBranchRange").forGetter(placer -> placer.maxBranchRange),
+            IntProvider.VALUE_CODEC.fieldOf("branchCount").forGetter(placer -> placer.branchCount),
+            FloatProvider.VALUE_CODEC.fieldOf("branchRange").forGetter(placer -> placer.branchRange),
+            FloatProvider.VALUE_CODEC.fieldOf("branchHeight").forGetter(placer -> placer.branchHeight),
             Codec.INT.fieldOf("base_height").forGetter(placer -> placer.baseHeight),
             Codec.INT.fieldOf("height_rand_a").forGetter(placer -> placer.firstRandomHeight),
             Codec.INT.fieldOf("height_rand_b").forGetter(placer -> placer.secondRandomHeight)
             ).apply(instance, WisteriaTrunkPlacer::new));
 
-    private final IntProvider tries;
-    private final IntProvider xOffset, yOffset, zOffset;
-    private final FloatProvider secondaryGrowthChance;
+    private final IntProvider maxBranchRange, branchCount;
+    private final FloatProvider branchHeight, branchRange;
 
-    public WisteriaTrunkPlacer(IntProvider tries, IntProvider xOffset, IntProvider yOffset, IntProvider zOffset, FloatProvider secondaryGrowthChance, int baseHeight, int firstRandomHeight, int secondRandomHeight) {
+    public WisteriaTrunkPlacer(IntProvider maxBranchRange, IntProvider branchCount, FloatProvider branchRange, FloatProvider branchHeight, int baseHeight, int firstRandomHeight, int secondRandomHeight) {
         super(baseHeight, firstRandomHeight, secondRandomHeight);
-        this.tries = tries;
-        this.xOffset = xOffset;
-        this.yOffset = yOffset;
-        this.zOffset = zOffset;
-        this.secondaryGrowthChance = secondaryGrowthChance;
+        this.maxBranchRange = maxBranchRange;
+        this.branchCount = branchCount;
+        this.branchHeight = branchHeight;
+        this.branchRange = branchRange;
     }
 
     @Override
@@ -58,77 +61,54 @@ public class WisteriaTrunkPlacer extends TrunkPlacer {
 
         int firstHeight = random.nextInt(baseHeight) + baseHeight / 2 + 1;
 
-        for (int i = 0; i < firstHeight; i++) {
+        // Creat initial trunk
+        for (int i = 0; i <= firstHeight+1; i++) {
             getAndSetState(world, replacer, random, startPos.up(i), config);
         }
 
-        if(firstHeight > 4) {
-            for (Direction direction : Direction.values()) {
-                if(random.nextBoolean()) {
-                    var sideHeight = random.nextInt(firstHeight + 1) / 2;
-                    var sidePos = startPos.offset(direction);
-                    for (int i = 0; i <= sideHeight; i++) {
-                        getAndSetState(world, replacer, random, sidePos.up(i), config);
+        BlockPos trunkTop = startPos.up(firstHeight-2);
+        nodes.add(new FoliagePlacer.TreeNode(trunkTop.up(3), 0, false));
+
+        int offset, previous;
+        float a, b;
+        Direction dir, dir2;
+        int yOffset = 0;
+        for (int i = 0; i < branchCount.get(random); i++) {
+            offset = 1;
+            previous = 0;
+            a = branchHeight.get(random);
+            b = branchRange.get(random);
+            dir = randomDirection(random);
+            dir2 = random.nextBoolean() ? dir.rotateYClockwise() : dir.rotateYCounterclockwise();
+            while (offset <= maxBranchRange.getMax()) {
+                yOffset = trunkFunc(offset, a, b);
+                if (yOffset < 1) {
+                    break;
+                }
+                if (previous == yOffset) {
+                    getAndSetState(world, replacer, random, trunkTop.up(yOffset).offset(dir, offset).offset(dir2, offset/2), config);
+                } else {
+                    for (int y = previous+1; y <= yOffset; y++) {
+                        getAndSetState(world, replacer, random, trunkTop.up(y).offset(dir, offset).offset(dir2, offset/2), config);
                     }
                 }
+                offset++;
+                previous = yOffset;
             }
-        }
-
-        var firstPos = startPos.up(firstHeight).mutableCopy();
-
-        var builder = AStarManager.createBuilder();
-        builder.start(AStarManager.BlockPosProvider.simple(firstPos));
-        builder.goal(worldview -> {
-            var xOff = xOffset.get(random) * (random.nextBoolean() ? -1 : 1);
-            var yOff = yOffset.get(random);
-            var zOff = zOffset.get(random) * (random.nextBoolean() ? -1 : 1);
-            return firstPos.add(xOff, yOff, zOff);
-        });
-        //builder.costMapper(AStarManager.Builder.c_favorReplaceable);
-        builder.allowRecompute();
-        //builder.allowDiagonalMovement();
-        var pathfinder = builder.build((WorldView) world);
-
-        var branches = tries.get(random);
-        var secondaryGrowth = random.nextFloat() <= secondaryGrowthChance.get(random);
-
-        if(random.nextBoolean()) {
-            nodes.add(new FoliagePlacer.TreeNode(firstPos, 0, false));
-        }
-
-        for (int i = 0; i < branches / (secondaryGrowth ? 2 : 1); i++) {
-            pathfinder.compute();
-            pathfinder.getLastOutput().ifPresent(pathingOutput -> {
-
-                nodes.add(new FoliagePlacer.TreeNode(pathingOutput.path().get(0).pos(), 0, false));
-
-                for (AStarManager.Node node : pathingOutput.path()) {
-                    getAndSetState(world, replacer, random, node.pos(), config);
-                }
-            });
-        }
-
-        if(secondaryGrowth) {
-            int secHeight = firstHeight / 3 + random.nextInt(secondRandomHeight);
-
-            for (int i = 0; i < secHeight; i++) {
-                getAndSetState(world, replacer, random, firstPos, config);
-                firstPos.move(Direction.UP);
-            }
-
-            for (int i = 0; i < branches; i++) {
-                pathfinder.compute();
-                pathfinder.getLastOutput().ifPresent(pathingOutput -> {
-
-                    nodes.add(new FoliagePlacer.TreeNode(pathingOutput.path().get(0).pos(), 0, false));
-
-                    for (AStarManager.Node node : pathingOutput.path()) {
-                        getAndSetState(world, replacer, random, node.pos(), config);
-                    }
-                });
-            }
+            trunkTop = trunkTop.up();
+            nodes.add(new FoliagePlacer.TreeNode(trunkTop.up(previous-1).offset(dir, offset-2).offset(dir2, offset/4), 0, false));
         }
 
         return nodes;
     }
+
+    private int trunkFunc(float x, float a, float b) {
+        return (int) Math.ceil(-Math.log((2*a / x) - b) + 3);
+    }
+
+    private static Direction[] directions = new Direction[] {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
+    private Direction randomDirection(Random random) {
+        return directions[random.nextInt(directions.length)];
+    }
+
 }
