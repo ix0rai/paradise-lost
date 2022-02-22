6319423016d295dbf0ac947be63b4e4266b94527
Jack Papel <jackpapel@live.com>
2022-02-21 16:58:04 -0800

    MoaAPI changes
    Goodbye Function4, hello MoaBreedingContext!
    Change docs
    Simplify breeding and spawning registration
    Privatize things that shouldn't be seen
    Changed `getMoaForBiome` to `getMoaFromBiome`. I didn't deprecate anything for that. I figure they can figure out a 2 letter difference.
    Simplified down MatingEntry. Now has just one predicate instead of two.
    MOA_BREEDING_REGISTRY can be replaced with a map, removing MatingEntry. It ultimately wouldn't affect anyone using the API, but I think it's fine the way it is.
    Moved SpawnStatData inside SpawnStatWeighting, renamed to Weighting.
    Register the Fallback Moa to prevent any issues down the road.
    MoaEgg command now suggests the fallback moa as a consequence, but I've added proper support for it.
    The fallback moa now has its own texture, though it is identical to the highlands blue moa texture.
    Changed MoaRace registration to be a better example for others. No more weird varargs and actions and whatnot.
    Ooh, and just look at that spawning and breeding registration! I got it all lined up. Pretty proud of how it looks.
    This should be backported to the snapshots branch.

--- src/main/java/net/id/aether/commands/MoaEggCommand.java
+++ src/main/java/net/id/aether/commands/MoaEggCommand.java
@@ -24,13 +24,13 @@ import static net.minecraft.server.command.CommandManager.literal;
 
 public class MoaEggCommand {
 
-    private static final RaceSuggester RACES = new RaceSuggester();
+    private static final MoaRaceSuggester MOA_RACES = new MoaRaceSuggester();
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(
                 literal("moaegg")
                         .requires((source) -> source.hasPermissionLevel(2))
-                        .then(argument("target", EntityArgumentType.players()).then((argument("race", IdentifierArgumentType.identifier()).suggests(RACES)
+                        .then(argument("target", EntityArgumentType.players()).then((argument("race", IdentifierArgumentType.identifier()).suggests(MOA_RACES)
                                 .executes(context -> execute(context.getSource(), EntityArgumentType.getPlayers(context, "target"), IdentifierArgumentType.getIdentifier(context, "race"), false)))
                                 .then(literal("asBaby")
                                         .executes(context -> execute(context.getSource(), EntityArgumentType.getPlayers(context, "target"), IdentifierArgumentType.getIdentifier(context, "race"), true)))))
@@ -40,7 +40,8 @@ public class MoaEggCommand {
     private static int execute(ServerCommandSource source, Collection<ServerPlayerEntity> targets, Identifier raceId, boolean baby) {
         var race = MoaAPI.getRace(raceId);
 
-        if (race == MoaAPI.FALLBACK_MOA) {
+        // Let them get the fallback moa if they intend to, otherwise fail.
+        if (race == MoaAPI.FALLBACK_MOA && !raceId.equals(MoaAPI.FALLBACK_MOA.getId())) {
             source.sendError(new TranslatableText("commands.the_aether.moaegg.failure", raceId.toString()));
         } else {
             ItemStack template = MoaGenes.getEggForCommand(race, source.getWorld(), baby);
@@ -56,8 +57,8 @@ public class MoaEggCommand {
         return targets.size();
     }
 
-    public static class RaceSuggester implements SuggestionProvider<ServerCommandSource> {
-
+    public static class MoaRaceSuggester implements SuggestionProvider<ServerCommandSource> {
+        // This suggests the fallback moa too now. If this isn't desired, a check can be added.
         @Override
         public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) {
             MoaAPI.getRegisteredRaces().forEachRemaining(race -> builder.suggest(race.getId().toString()));
